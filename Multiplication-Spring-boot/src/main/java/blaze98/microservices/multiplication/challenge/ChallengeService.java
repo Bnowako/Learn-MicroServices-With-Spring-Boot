0febe792951209d6b10edabbd5671bbafb998616
blaze98 <bnowakowski98@gmail.com>
2021-04-01 18:39:32 +0200

    commit

--- Multiplication-Spring-boot/src/main/java/blaze98/microservices/multiplication/challenge/ChallengeService.java
+++ Multiplication-Spring-boot/src/main/java/blaze98/microservices/multiplication/challenge/ChallengeService.java
@@ -1,5 +1,8 @@
 package blaze98.microservices.multiplication.challenge;
 
+import java.util.List;
+
 public interface ChallengeService {
     ChallengeAttempt verifyAttempt(ChallengeAttemptDto resultAttempt);
+    List<ChallengeAttempt> getStatsForUser(String userAlias);
 }
